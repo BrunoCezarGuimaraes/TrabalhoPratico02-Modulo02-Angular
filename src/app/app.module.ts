1a3091014f02b77e7a621d2d0fe33710c944a945
Bruno Cezar Guimarães <bruno_guimaraes007@hotmail.com>
2021-06-22 19:57:50 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PedidoComponent } from './pedido/pedido.component';
+import { CardapioComponent } from './cardapio/cardapio.component';
+
+import { HttpClientModule } from "@angular/common/http";
+
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatBadgeModule } from "@angular/material/badge";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedidoComponent,
+    CardapioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
